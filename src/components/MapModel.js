f92b381571d618621f84517742e807aff07cd84f
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-21 11:28:49 +0600

    project done

--- src/components/MapModel.js
+++ src/components/MapModel.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import getImageURL from "../utils/getimageurl";
 import Button from "./Button";
 import ThreeDContainer from './ThreeDContainer';
