5271303b4351eb021b721ad48bcdb29109256fca
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-21 11:17:34 +0600

    create mapmodel

---
+++ src/components/MapModel.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import getImageURL from "../utils/getimageurl";
+import Button from "./Button";
+import ThreeDContainer from './ThreeDContainer';
+
+const MapModel = () => {
+  const { lat, lng, zoom } = useParams();
+
+  const { url1, url2 } = getImageURL(lng, lat, zoom);
+
+  return (
+    <div className="container">
+      <ThreeDContainer url1={url1} url2={url2} />
+      <Button text="Go to Map" linkTo="/" />
+    </div>
+  )
+}
+
+export default MapModel;
